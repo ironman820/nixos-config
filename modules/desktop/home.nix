f4b9c6831507106ae9c8b47bd6672989eeff663d
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-05-11 19:28:04 -0500

    feat: :lipstick: Added XFCE Config

--- modules/desktop/home.nix
+++ modules/desktop/home.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+    home.file = {
+        ".config/xfce4/panel" = {
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/xfce4/panel";
+            recursive = true;
+        };
+        ".config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/xfce4/xfce4-keyboard-shortcuts.xml";
+        ".config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/xfce4/xfce4-panel.xml";
+    };
     gtk = {
         enable = true;
         theme = {
--- users/royell/home.nix
+++ users/royell/home.nix
@@ -2,8 +2,10 @@
 
 {
     home = {
-        file.".ssh/id_rsa.pub".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/royell/ssh/id_rsa.pub";
-        file.".ssh/id_rsa".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/secrets/home/royell/ssh/id_rsa";
+        file = {
+            ".ssh/id_rsa.pub".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/royell/ssh/id_rsa.pub";
+            ".ssh/id_rsa".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/secrets/home/royell/ssh/id_rsa";
+        };
         homeDirectory = "/home/royell";
         username = "royell";
     };
