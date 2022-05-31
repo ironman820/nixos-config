2f57c30e0b5c58d9e32b58f88233d5aa46a20163
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-05-30 22:59:22 -0500

    refactor: :wrench: Enabled git GPG signing

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -1,7 +1,11 @@
 {config, ...}:
 {
-    programs.git.extraConfig.user = {
-        name = "Nicholas Eastman";
-        email = "29488820+ironman820@users.noreply.github.com";
+    programs.git.extraConfig = {
+        commit.gpgsign = true;
+        user = {
+            name = "Nicholas Eastman";
+            email = "29488820+ironman820@users.noreply.github.com";
+            signingkey = "9F30DA1A16D74EA7";
+        };
     };
 }
