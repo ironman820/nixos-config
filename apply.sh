3413eb8a520364eb94ad6408361f5fc0c82866e1
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-05-30 23:48:13 -0500

    fix: :bug: Fixed GPG install script

--- apply.sh
+++ apply.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env sh
-pushd ~/dotfiles
+pushd ~/dotfiles > /dev/null
 sudo nixos-rebuild switch --flake .# --show-trace
 # nix build ".#hmConfig.$USER.activationPackage"
 # result/activate
-popd
+popd > /dev/null
 
-pushd ~
-if [ -f "wallpapers.7z.001" ]; then
-    7z x -y wallpapers.7z.001
-    rm wallpapers.7z.00*
+pushd ~ > /dev/null
+if [ -f "~/.walls-installed" ]; then
+    echo "Wallpapers flagged as installed, skipping extraction."
+else
+    if [ -f "wallpapers.7z.001" ]; then
+        7z x -y wallpapers.7z.001
+    fi
+    touch ~/.walls-installed
 fi
+rm ~/wallpapers.7z.00* 2>&1 > /dev/null
 chmod 600 .ssh/id_rsa
-popd
+popd > /dev/null
 
-pushd ~/personal-gpg
-./personal-gpg/install.sh
-popd
+pushd ~/personal-gpg > /dev/null
+# chmod +x install.sh
+./install.sh
+popd > /dev/null
--- modules/gpg/home.nix
+++ modules/gpg/home.nix
@@ -5,13 +5,14 @@
         file = {
             "personal-gpg/11B0F08E0A4D904B.public.asc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/gpg/keys/11B0F08E0A4D904B.public.asc";
             # "personal-gpg/11B0F08E0A4D904B.stubs.asc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/secrets/home/global/gpg/11B0F08E0A4D904B.stubs.asc";
-            "personal-gpg/install.sh".text = ''
-#!/bin/env sh
+            "personal-gpg/install.sh" = {
+                executable = true;
+                text = ''
+#!/usr/bin/env sh
 
 gpg -K --keyid-format=long | grep 9F30DA1A16D74EA7 > /dev/null
-if [ $? != 0]
-then
-    gpg --import 11B0F08E0A4D904B.public.asc
+if [ $? != 0 ]; then
+    gpg --import ~/personal-gpg/11B0F08E0A4D904B.public.asc
     echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key 11B0F08E0A4D904B trust;
     echo "If you don't have your GPG Card, the next phase will error out."
     echo "If that happens, run gpg --card-status once your have your card connected."
@@ -20,7 +21,8 @@ then
 
     echo "Finished!"
 fi
-            '';
+                '';
+            };
         };
     };
 }
