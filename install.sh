faac5e10b97c69c500b66824b801d8c4eb44f565
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-06-28 16:10:31 -0500

    feat: :recycle: Unlock repo in home before moving to etc

--- install.sh
+++ install.sh
@@ -17,14 +17,55 @@ if [ $? != 0 ]; then
   nix-shell --command "./install.sh"
 else
 
-  echo "Cloning the repo to the NixOS directory at '/mnt/etc/nixos'"
+  echo "Cloning the repo."
 
   if [[ ! -d "/mnt/etc/nixos" ]]; then
-    sudo git clone -b system --depth 1 https://github.com/ironman820/nixos-config.git /mnt/etc/nixos/ 2>&1 > /dev/null
+    sudo git clone -b system --depth 1 https://github.com/ironman820/nixos-config.git ~/nixos 2>&1 > /dev/null
+    cd /nixos
   else
     cd /mnt/etc/nixos
     sudo git restore . 2>&1 > /dev/null
     sudo git pull 2>&1 > /dev/null
+  fi
+
+  if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
+    if gpg -k --keyid-format LONG | grep -iq 11B0F08E0A4D904B ; then
+      echo "Public GPG key found, skipping import."
+    else
+      echo "Please wait while we retrieve your GPG key..."
+      gpg --receive-keys 185B6FE0AC2034D0EB2F0ADD11B0F08E0A4D904B > /dev/null 2>&1
+      echo "Updating trust level"
+      echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key 11B0F08E0A4D904B trust > /dev/null;
+    fi
+    if gpg -K --keyid-format=long | grep -iq 9F30DA1A16D74EA7 ; then
+      echo "Private GPG key found, skipping import."
+    else
+      echo "If you don't have your GPG Card, the next phase will error out."
+      echo "If that happens, run 'gpg --card-status' once your have your card connected."
+      read -sp "Please insert your GPG Card and press 'Enter' to continue..."
+      gpg --card-status > /dev/null 2>&1
+    fi
+
+    if gpg -K --keyid-format=long | grep -iq 9F30DA1A16D74EA7 ; then
+      git crypt unlock
+      if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
+        for FILE in `find .git-crypt/keys/default -type f`; do
+          if gpg --pinentry-mode=loopback --decrypt < $FILE | sudo tee unlock-key > /dev/null 2>&1; then
+            git crypt unlock unlock-key
+            rm -f unlock-key
+          fi
+        done
+        if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
+          echo "Secrets have not been unlocked!"
+          echo "Please check for errors and try running the script again."
+          exit
+        fi
+      fi
+      sudo mv ~/nixos /mnt/etc/
+    else
+      echo "Your private GPG key did not import properly, please try again."
+      exit
+    fi
   fi
 
   sudo nixos-generate-config --root /mnt
@@ -42,45 +83,6 @@ else
 
   sudo cp hosts/$hostname/configuration.nix ./
 
-  if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
-    if sudo gpg -k --keyid-format LONG | grep -iq 11B0F08E0A4D904B ; then
-      echo "Public GPG key found, skipping import."
-    else
-      echo "Please wait while we retrieve your GPG key..."
-      sudo gpg --receive-keys 185B6FE0AC2034D0EB2F0ADD11B0F08E0A4D904B > /dev/null 2>&1
-      echo "Updating trust level"
-      echo -e "5\ny\n" | sudo gpg --command-fd 0 --expert --edit-key 11B0F08E0A4D904B trust > /dev/null;
-    fi
-    if sudo gpg -K --keyid-format=long | grep -iq 9F30DA1A16D74EA7 ; then
-      echo "Private GPG key found, skipping import."
-    else
-      echo "If you don't have your GPG Card, the next phase will error out."
-      echo "If that happens, run 'gpg --card-status' once your have your card connected."
-      read -sp "Please insert your GPG Card and press 'Enter' to continue..."
-      sudo gpg --card-status > /dev/null 2>&1
-    fi
-
-    if sudo gpg -K --keyid-format=long | grep -iq 9F30DA1A16D74EA7 ; then
-      sudo git crypt unlock
-      if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
-        for FILE in `find .git-crypt/keys/default -type f`; do
-          if sudo gpg --pinentry-mode=loopback --decrypt < $FILE | sudo tee unlock-key > /dev/null 2>&1; then
-            sudo git crypt unlock unlock-key
-            sudo rm -f unlock-key
-          fi
-        done
-        if file -bL --mime-encoding secrets/secrets.nix | grep -iq binary ; then
-          echo "Secrets have not been unlocked!"
-          echo "Please check for errors and try running the script again."
-          exit
-        fi
-      fi
-    else
-      echo "Your private GPG key did not import properly, please try again."
-      exit
-    fi
-  fi
-
   sudo sed -i 's/INSTALL_ROOT/\/mnt/' configuration.nix
   sudo sed -i 's/INSTALL_ROOT/\/mnt/' hardware-configuration.nix
   sudo sed -i 's/INSTALL_ROOT/\/mnt/' hosts/core.nix
@@ -91,8 +93,8 @@ else
   sudo sed -i 's/HOST_NAME/'"$hostname"'/' hardware-configuration.nix
 
   sudo mkdir -p /mnt/tmpstore/{work,store,tmp}
-  sudo export TMP=/mnt/tmpstore/tmp
-  sudo export TMPDIR=/mnt/tmpstore/tmp
+  export TMP=/mnt/tmpstore/tmp
+  export TMPDIR=/mnt/tmpstore/tmp
   sudo mount -t overlay overlay -olowerdir=/nix/store,upperdir=/mnt/tmpstore/store,workdir=/mnt/tmpstore/work /nix/store
 
   sudo nixos-install --no-root-passwd
