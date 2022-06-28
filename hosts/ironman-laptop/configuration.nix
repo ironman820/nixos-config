343089e5b47a54309a73fc3c8b067d927205d88f
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-06-28 06:50:59 -0500

    feat: :sparkles: Ironman-laptop

---
+++ hosts/ironman-laptop/configuration.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+let
+  qt5Packages = with pkgs.libsForQt5; [
+  ];
+
+  sysPackages = with pkgs; [
+  ];
+
+  unstablePackages = with pkgs.unstable; [
+  ];
+
+  xfcePackages = with pkgs.xfce; [
+  ];
+
+in
+
+{
+  # pkgs.overlays = [ inputs.nur.overlay ];
+  imports = [
+    INSTALL_ROOT/etc/nixos/hosts/core.nix
+    INSTALL_ROOT/etc/nixos/hosts/laptop.nix
+    INSTALL_ROOT/etc/nixos/users/ironman
+  ];
+
+  environment = {
+    etc = {
+      "ssh/ssh_host_ed25519_key" = {
+        mode = "0400";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_ed25519_key";
+      };
+      "ssh/ssh_host_ed25519_key.pub" = {
+        mode = "0444";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_ed25519_key.pub";
+      };
+      "ssh/ssh_host_rsa_key" = {
+        mode = "0400";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_rsa_key";
+      };
+      "ssh/ssh_host_rsa_key.pub" = {
+        mode = "0444";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_rsa_key.pub";
+      };
+      "ssh/ssh_host_dsa_key" = {
+        mode = "0400";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_dsa_key";
+      };
+      "ssh/ssh_host_dsa_key.pub" = {
+        mode = "0444";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_dsa_key.pub";
+      };
+      "ssh/ssh_host_ecdsa_key" = {
+        mode = "0400";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_ecdsa_key";
+      };
+      "ssh/ssh_host_ecdsa_key.pub" = {
+        mode = "0444";
+        source = "INSTALL_ROOT/etc/nixos/secrets/keys/ironman-laptop/ssh_host_ecdsa_key.pub";
+      };
+    };
+    systemPackages = qt5Packages ++ sysPackages ++ unstablePackages ++ xfcePackages;
+  };
+
+  networking = {
+    hostName = "ironman-laptop";
+  };
+}
