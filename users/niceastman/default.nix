f78ea661b02dfe6324d48a24e902244850ec6a45
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2022-06-28 21:23:43 -0500

    feat: Added user GECOS name

--- users/niceastman/default.nix
+++ users/niceastman/default.nix
@@ -20,6 +20,7 @@
   };
 
   users.users.niceastman = {
+    description = "Nicholas Eastman";
     isNormalUser = true;
     hashedPassword = "$6$PY0A75x0UUOjhBJN$UsGQGfBQMee.N.eenQTJLISYT5rTJpbDBI0GTdLx60L11R8c9m4x56mM2dmL2qSz4vvzGxN63Clk5NGVtKn9J0";
     extraGroups = [
